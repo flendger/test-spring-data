7d79425d7a585a79970f685f7bfad78dd0ba3031
flendger <kiru.ee@yandex.ru>
2022-05-14 13:37:58 +0300

    added repository test

---
+++ src/test/java/ru/flendger/testspringdata/service/IncomingMessageServiceTest.java
@@ -0,0 +1,18 @@
+package ru.flendger.testspringdata.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class IncomingMessageServiceTest {
+    @Autowired
+    private IncomingMessageService incomingMessageService;
+
+    @Test
+    public void insertTest() {
+
+    }
+}
