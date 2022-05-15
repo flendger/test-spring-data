8dd00e0dae1f6ea19dae9050f94e3f53e5274849
flendger <kiru.ee@yandex.ru>
2022-05-15 20:27:17 +0300

    added IncomingMessageServiceTest

--- src/test/java/ru/flendger/testspringdata/service/IncomingMessageServiceTest.java
+++ src/test/java/ru/flendger/testspringdata/service/IncomingMessageServiceTest.java
@@ -1,14 +1,19 @@
 package ru.flendger.testspringdata.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.flendger.testspringdata.model.IncomingMessage;
+import ru.flendger.testspringdata.model.MessageStatus;
 
-import java.util.Objects;
-import java.util.Queue;
-import java.util.concurrent.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +22,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class IncomingMessageServiceTest {
     @Autowired
     private IncomingMessageService incomingMessageService;
+
+    @Autowired
+    private IncomingMessageHandler handler;
+
+    private final List<IncomingMessage> sourceMessages = new ArrayList<>();
 
     @Test
     public void insertTest() {
@@ -76,9 +86,59 @@ class IncomingMessageServiceTest {
         messages.forEach(incomingMessageService::delete);
     }
 
+    @Test
+    public void updateTest() {
+        for (int i = 0; i < 100; i++) {
+            sourceMessages.add(incomingMessageService.save(generateMessage("uuid_" + 100 + i)));
+        }
+
+        Queue<IncomingMessage> handledMessages = new ConcurrentLinkedQueue<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        for (int i = 0; i < 4; i++) {
+            executorService.submit(getProgressNext(handledMessages));
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(100, handledMessages.size());
+
+        Map<String, List<IncomingMessage>> messagesByUuid =
+                handledMessages
+                        .stream()
+                        .collect(Collectors.groupingBy(IncomingMessage::getExternalId));
+
+        messagesByUuid.forEach((uuid, incomingMessages) -> assertEquals(1, incomingMessages.size()));
+    }
+
+    @AfterEach
+    void tearDown() {
+        sourceMessages.forEach(incomingMessageService::delete);
+    }
+
+    private Runnable getProgressNext(Queue<IncomingMessage> handledMessages) {
+        return () -> {
+            while (true) {
+                try {
+                    IncomingMessage message = handler.handleNext();
+                    handledMessages.add(message);
+                } catch (Exception e) {
+                    break;
+                }
+            }
+        };
+    }
+
     private IncomingMessage generateMessage(String externalId) {
         IncomingMessage message = new IncomingMessage();
         message.setExternalId(externalId);
+        message.setStatus(MessageStatus.PENDING);
 
         return message;
     }
